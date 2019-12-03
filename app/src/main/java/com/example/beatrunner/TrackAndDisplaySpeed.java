97b2e01934e2924b325fd124c91d46f06b85f4a8
Max-Qi <max.qi@mail.utoronto.ca>
2019-12-03 15:14:42 -0500

    Got step detector working

--- app/src/main/java/com/example/beatrunner/TrackAndDisplaySpeed.java
+++ app/src/main/java/com/example/beatrunner/TrackAndDisplaySpeed.java
@@ -4,31 +4,66 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public abstract class TrackAndDisplaySpeed extends AppCompatActivity implements SensorEventListener {
+import org.w3c.dom.Text;
 
-    public static float SPEED;
-    private SensorManager sensorManager;
-    private Sensor sensor;
+public class TrackAndDisplaySpeed extends AppCompatActivity implements SensorEventListener {
+
+    TextView stepsDisplay;
+    Button resumeOrPause;
+
+    SensorManager sensorManager;
+    Vibrator vibrator;
+
+    int steps = 0;
+
+    private String RESET = "Reset";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_track_and_display_speed);
-        countSteps();
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        stepsDisplay = (TextView) findViewById(R.id.stepDisplay);
+        resumeOrPause = (Button) findViewById(R.id.stopCounting);
     }
 
-    private void countSteps() {
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
-        boolean going = true;
-        while (going) {
-
+    protected void onResume() {
+        super.onResume();
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+        if (sensor != null) {
+            sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_FASTEST);
+        } else {
+            Toast.makeText(this, "Sensor not found!", Toast.LENGTH_SHORT).show();
         }
+    }
 
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+//        VibrationEffect vibrationEffect = VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE);
+        vibrator.vibrate(50);
+        steps ++;
+        stepsDisplay.setText(Integer.toString(steps));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
 }
