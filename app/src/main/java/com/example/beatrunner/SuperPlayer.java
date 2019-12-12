8d7c927f228ea235f160f4e985d5c27677336156
Max-Qi <max.qi@mail.utoronto.ca>
2019-12-11 23:48:11 -0500

    got started on player

---
+++ app/src/main/java/com/example/beatrunner/SuperPlayer.java
@@ -0,0 +1,68 @@
+package com.example.beatrunner;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class SuperPlayer extends AppCompatActivity {
+
+    private boolean hasAllPermissions = false;
+    AudioManager audioManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_super_player);
+        getPermissions();
+        if (hasAllPermissions) {
+            initialize();
+        }
+    }
+
+    private void getPermissions() {
+        String[] permissions = { Manifest.permission.MODIFY_AUDIO_SETTINGS };
+        for (String permission:permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, permissions, 0);
+                return;
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if ((requestCode != 0 || grantResults.length < 1 || grantResults.length != permissions.length)) {
+            return;
+        }
+        hasAllPermissions = true;
+        for (int grantResult:grantResults) {
+            if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                hasAllPermissions = false;
+                Toast.makeText(getApplicationContext(), "Please allow!", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void initialize() {
+        String sampleRateString = null;
+        String buffersizeString = null;
+        audioManager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
+        if (audioManager != null) {
+            sampleRateString = audioManager.getProperty(AudioManager.PROPERTY_OUTPUT_SAMPLE_RATE);
+            buffersizeString = audioManager.getProperty(AudioManager.PROPERTY_OUTPUT_FRAMES_PER_BUFFER);
+            if (sampleRateString == null) sampleRateString = "48000";
+            if (buffersizeString == null) buffersizeString = "4800";
+        }
+        int sampleRate = Integer.parseInt(sampleRateString);
+        int bufferSize = Integer.parseInt(buffersizeString);
+    }
+}
--- app/src/main/jni/cpp/BeatRunner.cpp
+++ app/src/main/jni/cpp/BeatRunner.cpp
@@ -1,3 +1,4 @@
+#include <BeatRunner.h>
 #include <jni.h>
 #include <string>
 #include <OpenSource/SuperpoweredAndroidAudioIO.h>
@@ -8,6 +9,11 @@
 
 static SuperpoweredAndroidAudioIO *audioIO;
 static Superpowered::Reverb *reverb;
+
+extern "C" JNIEXPORT void
+Java_com_example_beatrunner_MainActivity_BeatRunner () {
+
+}
 
 extern "C" JNIEXPORT void
 Java_com_example_beatrunner_MainActivity_StopAudio (JNIEnv * __unused env, jobject __unused obj) {
